f93335afbb1bd55007e27b3eb927826bf9cc3678
ikl7895 <littlechen777@gmail.com>
2025-01-24 22:05:33 +0800

    Create firebase_options.dart

---
+++ app/lib/firebase_options.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError(
+        'No Web options have been provided yet - configure Firebase for Web first',
+      );
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        throw UnsupportedError(
+          'No iOS options have been provided yet - configure Firebase for iOS first',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIza...',
+    appId: '1:1234...',
+    messagingSenderId: '1234...',
+    projectId: 'your-project-id',
+    storageBucket: 'your-project.appspot.com',
+  );
+} 
